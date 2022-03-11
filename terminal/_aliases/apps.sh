2083bae754e5f4e7feedf15abaf0e8f28ba1c7a8
finkin <arlekias@gmail.com>
2022-03-11 11:00:29 +0100

    [alias] Fix java11

--- terminal/_aliases/apps.sh
+++ terminal/_aliases/apps.sh
@@ -7,7 +7,7 @@ alias hoplon-off='/usr/bin/osascript -e "tell application \"Tunnelblick.app\"" -
 alias heimdall='/usr/bin/osascript -e "tell application \"Tunnelblick\"" -e "connect \"Heimdall\"" -e "end tell"'
 alias heimdall-off='/usr/bin/osascript -e "tell application \"Tunnelblick.app\"" -e "disconnect \"Heimdall\"" -e "end tell"'
 alias java8='export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"'
-alias java11='export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home"'
+alias java11='export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home"'
 alias java12='export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home"'
 alias java13='export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home"'
 alias typora="open -a typora"
